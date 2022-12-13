025c2d153dd1f212f42f625f8fc77ed50295fb0e
marcosbilobram <marcosbilobram@hotmail.com>
2022-12-13 17:47:54 -0300

    Creating quizQuestionActivity

---
+++ app/src/main/java/com/example/myquizapp/QuizQuestionsActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myquizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class QuizQuestionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_questions)
+
+
+
+        }
+}
