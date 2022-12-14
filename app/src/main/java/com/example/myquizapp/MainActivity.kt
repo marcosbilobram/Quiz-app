0c55dc5f1856b46fcbe5b04ba314698e0f285de0
marcosbilobram <marcosbilobram@hotmail.com>
2022-12-13 22:33:54 -0300

    Add flag images, creating the question model, setting questions

--- app/src/main/java/com/example/myquizapp/MainActivity.kt
+++ app/src/main/java/com/example/myquizapp/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.myquizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart : Button = findViewById(R.id.btnStart)
+        val textName : EditText = findViewById(R.id.text_name)
+
+        btnStart.setOnClickListener {
+            if(textName.text.isEmpty()){
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/myquizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/myquizapp/QuizQuestionsActivity.kt
@@ -2,14 +2,15 @@ package com.example.myquizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.util.Log
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
-
+        val questionsList = Constants.getQuestions()
+        Log.i("QuestionsList size is", "${questionsList.size}")
 
         }
 }
